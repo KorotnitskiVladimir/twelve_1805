71d2f1c1b8ed3afdfe5c26e0b2429539bcf189e2
Vladimir <korotnitskivladimir@gmail.com>
2024-05-18 11:28:51 +0300

    header added. function added

---
+++ twelve_1805/task.h
@@ -0,0 +1,6 @@
+﻿#include <iostream>
+using namespace std;
+void myFunc()
+{
+    cout << "this simple function shows text" << endl;
+}
--- twelve_1805/twelve_1805.cpp
+++ twelve_1805/twelve_1805.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
-using namespace std;
+#include "task.h"
 
 int main(int argc, char* argv[])
 {
     cout << "Hello, World!" << endl;
     cout << "New line" << endl;
+    myFunc();
     return 0;
 }
