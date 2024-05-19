edcf5b4feb26b93cd6b4327536a4edd2dd8ad6da
Vladimir <korotnitskivladimir@gmail.com>
2024-05-19 14:43:21 +0300

    fractional class created

---
+++ twelve_1805/Fractioanal.h
@@ -0,0 +1,85 @@
+﻿#include <iostream>
+using namespace std;
+
+class fractioanal
+{
+private:
+    int c;
+    int d;
+public:
+    fractioanal()
+    {
+        this->c = 0;
+        this->d = 0;
+    }
+    fractioanal(int a , int b)
+    {
+        this->c = a;
+        this->d = b;
+    }
+    void init(int a, int b);
+    void show();
+    fractioanal add (fractioanal a, fractioanal b);
+    fractioanal diff (fractioanal a, fractioanal b);
+    fractioanal mult (fractioanal a, fractioanal b);
+    fractioanal div (fractioanal a, fractioanal b);
+};
+
+inline void fractioanal::init(int a, int b)
+{
+    this->c = a;
+    this->d = b;
+}
+
+inline void fractioanal::show()
+{
+    cout << this->c << "/" << this->d << endl;
+}
+
+inline fractioanal fractioanal::add(fractioanal a, fractioanal b)
+{
+    fractioanal c;
+    if (a.d==b.d)
+    {
+        c.c = a.c + b.c;
+        c.d = a.d;
+    }
+    else
+    {
+        c.d = a.d * b.d;
+        c.c = a.c * b.d + b.c * a.d;
+    }
+    return c;
+}
+
+inline fractioanal fractioanal::diff(fractioanal a, fractioanal b)
+{
+    fractioanal c;
+    if (a.d==b.d)
+    {
+        c.c = a.c - b.c;
+        c.d = a.d;
+    }
+    else
+    {
+        c.d = a.d * b.d;
+        c.c = a.c * b.d - b.c * a.d;
+    }
+    return c;
+}
+
+inline fractioanal fractioanal::mult(fractioanal a, fractioanal b)
+{
+    fractioanal c;
+    c.c = a.c * b.c;
+    c.d = a.d * b.d;
+    return c;
+}
+
+inline fractioanal fractioanal::div(fractioanal a, fractioanal b)
+{
+    fractioanal c;
+    c.c = a.c * b.d;
+    c.d = a.d * b.c;
+    return c;
+}
---
+++ twelve_1805/ht.cpp
@@ -0,0 +1,17 @@
+﻿#include "Fractioanal.h"
+
+int main()
+{
+    fractioanal a(4, 5);
+    fractioanal b;
+    fractioanal c;
+    b.init(1, 6);
+    a.show();
+    b.show();
+    //c = a.add(a, b);
+    //c = a.diff(a, b);
+    //c = a.mult(a, b);
+    c = a.div(a, b);
+    c.show();
+    return 0;
+}
